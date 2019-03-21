412fd55c6a176be64aabfde9e309c3696627e9dc
tMatSuZ <tmatsuzaki1@gmail.com>
2019-03-22 00:45:41 +0900

    spring-cloud-function化ではまる

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,40 +1,7 @@
 package com.serverless;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.apache.log4j.Logger;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+public class Handler extends SpringBootRequestHandler<String, String> {
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
-
-    private static final Logger LOG = Logger.getLogger(Handler.class);
-
-    @Override
-    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        LOG.info("received: " + input);
-        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
-
-        // dynamodb
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withRegion(Regions.AP_NORTHEAST_1)
-                .build();
-        HashMap<String, AttributeValue> item_values = new HashMap<String,AttributeValue>();
-        item_values.put("id", new AttributeValue("id"));
-        item_values.put("name", new AttributeValue("name"));
-        client.putItem("TestTable", item_values);
-
-        return ApiGatewayResponse.builder()
-                .setStatusCode(200)
-                .setObjectBody(responseBody)
-                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-                .build();
-    }
 }
---
+++ src/main/java/com/serverless/MyFunction.java
@@ -0,0 +1,52 @@
+package com.serverless;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Function;
+
+@SpringBootConfiguration
+public class MyFunction implements ApplicationContextInitializer<GenericApplicationContext> {
+
+    private static final Logger LOG = Logger.getLogger(MyFunction.class);
+
+    public static void main(String[] args) throws Exception {
+        FunctionalSpringApplication.run(MyFunction.class, args);
+    }
+
+    @Override
+    public void initialize(GenericApplicationContext context) {
+        context.registerBean("function", FunctionRegistration.class,
+                () -> new FunctionRegistration<Function<String, String>>(handleRequest())
+                        .type(FunctionType.from(String.class).to(String.class).getType()));
+    }
+
+
+    public Function<String, String> handleRequest() {
+        return value -> {
+          return value;
+        };
+//        LOG.info("received: " + input);
+//        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+//
+//        // dynamodb
+//        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+//                .withRegion(Regions.AP_NORTHEAST_1)
+//                .build();
+//        HashMap<String, AttributeValue> item_values = new HashMap<String,AttributeValue>();
+//        item_values.put("id", new AttributeValue("id"));
+//        item_values.put("name", new AttributeValue("name"));
+//        client.putItem("TestTable", item_values);
+//
+//        return ApiGatewayResponse.builder()
+//                .setStatusCode(200)
+//                .setObjectBody(responseBody)
+//                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+//                .build();
+    }
+}
