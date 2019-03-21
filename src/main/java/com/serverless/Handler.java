4a9faec80e9bea705b61461e0cc66b100a0bf6af
tMatSuZ <tmatsuzaki1@gmail.com>
2019-03-21 16:55:16 +0900

    dynamodb追加

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,25 +1,40 @@
 package com.serverless;
 
-import java.util.Collections;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
-
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = Logger.getLogger(Handler.class);
+    private static final Logger LOG = Logger.getLogger(Handler.class);
 
-	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: " + input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-				.build();
-	}
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("received: " + input);
+        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+
+        // dynamodb
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.AP_NORTHEAST_1)
+                .build();
+        HashMap<String, AttributeValue> item_values = new HashMap<String,AttributeValue>();
+        item_values.put("id", new AttributeValue("id"));
+        item_values.put("name", new AttributeValue("name"));
+        client.putItem("TestTable", item_values);
+
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(responseBody)
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                .build();
+    }
 }
